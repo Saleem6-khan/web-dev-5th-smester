12594ecf7de6741a87929adf83e028bd9608e26b
Saleem6-khan <muhammadsaleemullahkhan5426@gmail.com>
2025-10-23 23:44:44 +0500

    just updated

--- drum/Drum Kit - 101/drum.js
+++ drum/Drum Kit - 101/drum.js
@@ -2,86 +2,50 @@ const action = document.querySelectorAll(".container button");
 console.log(action);
 
 
-action.forEach((button) => {
-
-    button.addEventListener("keypress",(e)=>{
+action.forEach((btn) => {
+    btn.addEventListener("click",(e)=>{
         e.preventDefault();
-        const sound = e.key;  
+        const sound = e.target.id
         console.log(sound);
-        if(sound == 'a'){
-            let audio = new Audio('sounds/tom-2.mp3')
-    audio.play();
-    }
-    else if(sound == 's'){
-        let audio = new Audio('sounds/tom-3.mp3')
-        audio.play();
-    }
-    else if(sound == 'd'){
-        let audio = new Audio('sounds/tom-4.mp3')
-        audio.play();
-    }
-    else if(sound == 'j'){
-        let audio = new Audio('sounds/snare.mp3')
-        audio.play();
-    }
-    else if(sound == 'k'){
-        let audio = new Audio('sounds/crash.mp3')
-        audio.play();
-    }
-    else if(sound == 'l'){
-        let audio = new Audio('sounds/kick-bass.mp3')
-        audio.play();
-    }
-    else if(sound == 'w'){
-        let audio = new Audio('sounds/tom-1.mp3')
-        audio.play();
-    }
-    });
+        
+        PlaySound(sound);
+    })
+})
 
-
-    button.addEventListener("click",(e)=>{
-
-
-
-
-
-
-
-
-
-
-
-
-
-        e.preventDefault();
-        const sound = e.target.id;  // this gives you "a", "s", "d" etc
-
-        console.log(sound);
-        if(sound == 'a'){
-            let audio = new Audio('sounds/tom-2.mp3')
-    audio.play();
-    } else if(sound == 's'){
-        let audio = new Audio('sounds/tom-3.mp3')
-        audio.play();
-    } else if(sound == 'd'){
-        let audio = new Audio('sounds/tom-4.mp3')
-        audio.play();
-    } else if(sound == 'j'){
-        let audio = new Audio('sounds/snare.mp3')
-        audio.play();
-    } else if(sound == 'k'){
-        let audio = new Audio('sounds/crash.mp3')
-        audio.play();
-    } else if(sound == 'l'){
-        let audio = new Audio('sounds/kick-bass.mp3')
-        audio.play();
-    } else if(sound == 'w'){
-        let audio = new Audio('sounds/tom-1.mp3')
-        audio.play();
-
- }
- 
-});
+document.addEventListener("keydown", (e) => {
+  const sound = e.key; 
+  console.log(sound);
+  PlaySound(sound);
 });
 
 
+function PlaySound(sound){
+    if(sound === "w"){
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+    }else if(sound === "a"){    
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+    }else if(sound === "s"){
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+    }
+    else if(sound === "d"){
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+    }
+    else if(sound === "j"){
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+    }   
+    else if(sound === "k"){
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+    }
+    else if(sound === "l"){
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+    }
+
+    
+}
